d691ec88e86410fb56a67c033e9d854a719fe40b
perfectFu <fuaoqi@outlook.com>
2020-09-25 18:17:40 +0800

    添加两数相加、无重复字符的最长子串

--- 2.两数相加.js
+++ 2.两数相加.js
@@ -17,8 +17,31 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function(l1, l2) {
 
+function ListNode(val) {
+	this.val = val;
+	this.next = null;
+}
+var addTwoNumbers = function (l1, l2) {
+	const dummyHead = new ListNode();
+	let p = l1,
+		q = l2,
+		curr = dummyHead;
+	let curry = 0; // 进位，判断两值相加是否超过10
+	while (p !== null || q !== null) {
+		let x = p !== null ? p.val : 0;
+		let y = q !== null ? q.val : 0;
+		let sum = x + y + curry;
+		curry = parseInt(sum / 10); // 设置下一步的进位
+		curr.next = new ListNode(sum % 10);
+		curr = curr.next;
+		if (p !== null) p = p.next;
+		if (q !== null) q = q.next;
+	}
+	// 如果最后一位相加大于10
+	if (curry > 0) {
+		curr.next = new ListNode(curry);
+	}
+	return dummyHead.next;
 };
 // @lc code=end
-
--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -9,8 +9,25 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-
+// 采用滑动窗口方式实现
+var lengthOfLongestSubstring = function (s) {
+	const occ = new Set();
+	const n = s.length;
+	// 右指针，初始值为-1，相当于我们在字符串左边界的左侧还没有开始移动
+	let rk = -1,
+		ans = 0;
+	for (let i = 0; i < n; i++) {
+		if (i !== 0) {
+			occ.delete(s.charAt(i - 1));
+		}
+		while (rk + 1 < n && !occ.has(s.charAt(rk + 1))) {
+			occ.add(s.charAt(rk + 1));
+			rk++;
+		}
+		console.log('occ:', occ);
+		ans = Math.max(ans, rk - i + 1);
+	}
+	return ans;
 };
+lengthOfLongestSubstring('abcabcbb');
 // @lc code=end
-
